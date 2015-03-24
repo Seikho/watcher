cc8fc271814d87829aab7378ff74f6c33d4dec40
Seikho <carl@carlossus.com>
2015-03-24 11:59:10 +0800

    Fixed grunt watch task in gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -1,27 +1,26 @@
-function task(grunt) {
-grunt.initConfig({
-       ts:{ 
-           default:{
-               src: ["**/*.ts", "!node_modules/**/*.ts"],
-               verbose: false,
-               options: {
-                   module: "commonjs",
-                   target: "es5"
-               }
-           },
-           watch:{
-               src: ["**/*.ts", "!node_modules/**/*.ts"],
-               verbose: false,
-               options: {
-                   module: "commonjs",
-                   target: "es5"
-               },
-               watch: "src/**/*.ts"
-           }
-       }
+module.exports = function(grunt) {
+    grunt.initConfig({
+        ts: {
+            default: {
+                src: ["src/**/*.ts"],
+                options:{
+                    module: "commonjs",
+                    target: "es5"
+                }
+            },
+            watch:{
+                src: ["src/**/*.ts", "!node_modules/**/*.ts"],
+                verbose: false,
+                options: {
+                    module: "commonjs",
+                    target: "es5"
+                },
+                watch: "src/**/*.ts"
+            }
+        },
+
     });
     grunt.loadNpmTasks("grunt-ts");
-    grunt.registerTask("default", ["ts:default"]);
+    grunt.registerTask("default", ["ts"]);
     grunt.registerTask("watch", ["ts:watch"]);
-}
-module.exports = task;
+};
